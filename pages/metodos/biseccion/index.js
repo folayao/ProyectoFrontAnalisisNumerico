3c51b4fea51fcb39314f7fcd72ab99e9bb0a2eaf
Felipe Olaya Ospina <folayao@eafit.edu.co>
2022-05-31 19:26:10 -0500

    biseccion

--- pages/metodos/biseccion/index.js
+++ pages/metodos/biseccion/index.js
@@ -46,7 +46,7 @@ const Index = () => {
         <Inputs
           labelTitle={'tolerancia'}
           setFunction={setTol}
-          typeInput={'number'}
+          typeInput={'text'}
           value={tol}
         />
         <br />
